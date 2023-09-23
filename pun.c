0b829186a86ba5f260862f388910a04a88650e4d
Dugomsa Dedefo <zcowboii@gmail.com>
2023-09-23 13:21:03 +0300

    first commit

---
+++ pun.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /*Dude I can even write a fuc*n comment in C damn!!!!!*/
+
+    // int height, length, width, volume;
+    // float profit, loss, cost;
+
+    // height = 6;
+    // length = 4;
+    // width = 2;
+    // volume = 1;
+    // profit = 123.54;
+    // loss = 21.34;
+    // cost = 10.3;
+    // printf("Height: %d\n", height);
+    // printf("Profit: $%.2f\n", profit);
+    // printf("Volume: %d\n", height * length * width);
+    // printf("To C, or Not to C: that's the question.\n");
+    // printf("Brevity is the soul of wit. \n  --Shakespeare. \n");
+
+    // int height, length, width, volume, weight;
+    // length = 12;
+    // width = 10;
+    // height = 8;
+    // volume = height * width * length;
+    // weight = (volume + 165) / 166;
+    printf("Enter the width: ");
+    scanf("%d", &width);
+    printf("Dimensions: %dx%dx%d\n Volume: %d\n Dimensional Weight: %d\n\n", length, width, height, volume, weight);
+
+    return 0;
+}
